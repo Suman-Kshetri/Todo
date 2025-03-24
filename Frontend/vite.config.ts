312592e17b78b2859d6dc0e2012639850bcbf912
Suman-Kshetri <ksuman20601028@gmail.com>
2025-03-24 12:56:22 +0545

    feat(Frontend): integrate Tailwind CSS and update project assets

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 // Vite config with proxy setup
 export default defineConfig({
@@ -15,5 +16,7 @@ export default defineConfig({
       }
     },
   },
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react(),],
 });
