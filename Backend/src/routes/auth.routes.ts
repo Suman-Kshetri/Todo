35e0f9af7e891c523c5e7981098819f179fbc5da
Suman-Kshetri <ksuman20601028@gmail.com>
2025-07-14 18:00:44 +0545

    feat(google auth): 0auth

--- Backend/src/routes/auth.routes.ts
+++ Backend/src/routes/auth.routes.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user-controller";
 import { upload } from "../middlewares/multer.middleware";
 import { verifyJWT } from "../middlewares/auth.middleware";
-
+import passport from "passport";
+import { googleAuthCallback } from "../controllers/user-controller";
 
 const authRouter = Router()
 
@@ -19,6 +20,26 @@ authRouter.route("/login").post(loginUser)
 authRouter.route("/logout").post(verifyJWT, logoutUser)
 //verifyjwt --> next() --> logoutuser -> now the logoutuser has acces to user object that we added
 authRouter.route("/refreshToken").post(refreshAccessToken)
+
+
+// ---------------------------
+// 🔐 Google OAuth Routes
+// ---------------------------
+
+// Step 1: Redirect to Google Auth
+authRouter.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// Step 2: Google callback endpoint
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false }),
+  googleAuthCallback
+);
 
 
 export default authRouter;
